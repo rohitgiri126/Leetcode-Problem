39a5a255f5f15081a5a350bb410490ef0f0fe77c
Rohit kumar <rkg7852@gmail.com>
2026-07-23 11:42:18 +0530

    Time: 31 ms (14.15%) | Memory: 24.3 MB (5.81%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -5,18 +5,21 @@ class Solution:
         visited = set()
 
         def dfs(r, c):
-            if (r < 0 or r >= rows or 
-                c < 0 or c >= cols or 
-                grid[r][c] == 0 or 
-                (r, c) in visited):
-                return 0
-
             visited.add((r, c))
-
-            return (1 + dfs(r - 1, c) 
-                      + dfs(r + 1, c) 
-                      + dfs(r, c - 1) 
-                      + dfs(r, c + 1))
+            count = 1  
+            
+            directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+            
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+                
+                if (0 <= nr < rows and 0 <= nc < cols and 
+                    grid[nr][nc] == 1 and 
+                    (nr, nc) not in visited):
+                    
+                    count += dfs(nr, nc)
+                    
+            return count
 
         max_area = 0
         for r in range(rows):
