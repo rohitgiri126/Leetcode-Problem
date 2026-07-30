e7a5289d29a5b80546555f48959f54997176f129
Rohit kumar <rkg7852@gmail.com>
2026-07-30 20:11:40 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (51.48%) - LeetSync

--- 231-power-of-two/power-of-two.py
+++ 231-power-of-two/power-of-two.py
@@ -4,4 +4,7 @@ class Solution:
             return False
         while n%2==0:
             n=n//2
-        return n==1      
+        if n==1:
+            return True 
+        else:
+            return False        
