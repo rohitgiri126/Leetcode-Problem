ba02386ac612d2ad9d6efe19ac5f3c65f864d20e
Rohit kumar <rkg7852@gmail.com>
2026-08-06 12:03:31 +0530

    Time: 322 ms (58.82%) | Memory: 107.1 MB (58.82%) - LeetSync

---
+++ 3561-remove-methods-from-project/remove-methods-from-project.py
@@ -0,0 +1,22 @@
+class Solution:
+    def remainingMethods(self, n: int, k: int, invocations: List[List[int]]) -> List[int]:
+        adj = collections.defaultdict(list)
+        for u, v in invocations:
+            adj[u].append(v)
+            
+        suspicious = set()
+        queue = collections.deque([k])
+        suspicious.add(k)
+        
+        while queue:
+            node = queue.popleft()
+            for neighbor in adj[node]:
+                if neighbor not in suspicious:
+                    suspicious.add(neighbor)
+                    queue.append(neighbor)
+                    
+        for u, v in invocations:
+            if u not in suspicious and v in suspicious:
+                return list(range(n))
+                
+        return [i for i in range(n) if i not in suspicious]
