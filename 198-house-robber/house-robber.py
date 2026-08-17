60bb8dff944403b3c25f2d8e40985f66710bdf34
Rohit kumar <rkg7852@gmail.com>
2026-08-17 23:39:48 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (90.42%) - LeetSync

---
+++ 198-house-robber/house-robber.py
@@ -0,0 +1,6 @@
+class Solution:
+    def rob(self, nums: list[int]) -> int:
+        rob1, rob2 = 0, 0
+        for n in nums:
+            rob1, rob2 = rob2, max(rob1 + n, rob2)
+        return rob2
