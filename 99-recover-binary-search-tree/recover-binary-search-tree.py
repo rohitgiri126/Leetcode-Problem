d91c8eafadc252bbf951df935e4e8d073ac941bb
Rohit kumar <rkg7852@gmail.com>
2026-09-01 22:49:44 +0530

    Time: 0 ms (100.00%) | Memory: 20.4 MB (44.00%) - LeetSync

---
+++ 99-recover-binary-search-tree/recover-binary-search-tree.py
@@ -0,0 +1,23 @@
+class Solution:
+    def recoverTree(self, root: Optional[TreeNode]) -> None:
+        first = second = prev = None
+
+        def inorder(node: Optional[TreeNode]) -> None:
+            nonlocal first, second, prev
+            if not node:
+                return
+            
+            inorder(node.left)
+            
+            if prev and prev.val > node.val:
+                if not first:
+                    first = prev
+                second = node
+            prev = node
+            
+            inorder(node.right)
+
+        inorder(root)
+        
+        if first and second:
+            first.val, second.val = second.val, first.val
