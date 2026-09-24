b597fe1c1eb181ce1b1bf5887f23aacfa703f251
Rohit kumar <rkg7852@gmail.com>
2026-09-24 23:13:50 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (77.81%) - LeetSync

---
+++ 61-rotate-list/rotate-list.py
@@ -0,0 +1,25 @@
+class Solution:
+    def rotateRight(self, head: ListNode | None, k: int) -> ListNode | None:
+        if not head or not head.next or k == 0:
+            return head
+
+        length = 1
+        tail = head
+        while tail.next:
+            tail = tail.next
+            length += 1
+
+        k = k % length
+        if k == 0:
+            return head
+
+        steps_to_new_tail = length - k
+        new_tail = head
+        for _ in range(steps_to_new_tail - 1):
+            new_tail = new_tail.next
+
+        new_head = new_tail.next
+        new_tail.next = None
+        tail.next = head
+
+        return new_head
